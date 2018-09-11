be148b18d13c33eba731c740a09238d70ccf3a47
brandonocasey <brandonocasey@gmail.com>
2018-09-11 14:45:44 -0400

    chore: generator v7

--- src/unpad.js
+++ src/unpad.js
@@ -1,5 +1,6 @@
 /**
  * Returns the subarray of a Uint8Array without PKCS#7 padding.
+ *
  * @param padded {Uint8Array} unencrypted bytes that have been padded
  * @return {Uint8Array} the unpadded bytes
  * @see http://tools.ietf.org/html/rfc5652
