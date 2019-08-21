da31c291d1d83bcbe9f2a2b7a086d826bf1d9f35
Brandon Casey <2381475+brandonocasey@users.noreply.github.com>
2019-08-21 17:06:55 -0400

    chore: generator 7.7.3 update (#13)

--- scripts/rollup.config.js
+++ scripts/rollup.config.js
@@ -7,45 +7,32 @@ const config = generate(options);
 
 // Add additonal builds/customization here!
 
-config.builds.pad = config.makeBuild('browser', {
-  input: 'src/pad.js',
-  output: [{
-    name: 'pkcs7.pad',
-    file: 'dist/pkcs7.pad.js',
-    format: 'umd'
-  }]
-});
-
-config.builds.unpad = config.makeBuild('browser', {
-  input: 'src/unpad.js',
-  output: [{
-    name: 'pkcs7.unpad',
-    file: 'dist/pkcs7.unpad.js',
-    format: 'umd'
-  }]
-});
-
-if (config.builds.browserMin) {
-  config.builds.padMin = config.makeBuild('browser', {
+config.builds = Object.assign(config.builds, {
+  pad: config.makeBuild('browser', {
     input: 'src/pad.js',
     output: [{
       name: 'pkcs7.pad',
+      file: 'dist/pkcs7.pad.js',
+      format: 'umd'
+    }, {
+      name: 'pkcs7.pad',
       file: 'dist/pkcs7.pad.min.js',
       format: 'umd'
-    }],
-    plugins: config.builds.browserMin.plugins
-  });
-
-  config.builds.unpadMin = config.makeBuild('browser', {
+    }]
+  }),
+  unpad: config.makeBuild('browser', {
     input: 'src/unpad.js',
     output: [{
       name: 'pkcs7.unpad',
+      file: 'dist/pkcs7.unpad.js',
+      format: 'umd'
+    }, {
+      name: 'pkcs7.unpad',
       file: 'dist/pkcs7.unpad.min.js',
       format: 'umd'
-    }],
-    plugins: config.builds.browserMin.plugins
-  });
-}
+    }]
+  })
+});
 
 // export the builds to rollup
 export default Object.values(config.builds);
